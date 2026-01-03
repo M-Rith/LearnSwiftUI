244b0505c7a7857cc215e2e90f0c9414e76d1764
USER <user@USERs-MacBook-Pro.local>
2026-01-04 00:21:10 +0900

    finish add reset game

--- GuessTheWordsGame/GuessTheWordsGame/ContentView.swift
+++ GuessTheWordsGame/GuessTheWordsGame/ContentView.swift
@@ -48,9 +48,15 @@ struct ContentView: View {
             userGuess = guessArray.joined(separator: " ")
         } else {
             if !wrongCharacter.contains(letter) {
+                life -= 1
                 wrongCharacter.append(letter)
             }
         }
+    }
+    
+    func tryAgain() {
+        life = 5
+        getRandomWord()
     }
 
 
@@ -58,53 +64,82 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack (alignment: .center) {
-                if (guessWord == "") {
-                    Text("You have 5 hearts")
-                        .font(.title)
-                        .fontWeight(.bold)
-                    Text("Each wrong guess will lose 1 heart")
-                        .font(.headline)
-                    Text("You lose when heart each 0")
-                        .font(.headline)
-                
-                    Button(action: {
-                        getRandomWord()
-                    }, label: {
-                        Text("Started")
-                    })
-                    .padding(.top, 100)
-                    .padding(.bottom, 100)
-                }
-                else {
-                    VStack {
-                        HStack {
-                            Text("\(life) Remaining")
-                        }
-                        
-                        Text(userGuess)
+                if (life > 0) {
+                    if (guessWord == "") {
+                        Text("You have 5 hearts")
                             .font(.title)
                             .fontWeight(.bold)
-                        LazyVGrid(columns: columns, spacing: 12) {
-                              ForEach(characters, id: \.self) { letter in
-                                  Text(letter)
-                                     .font(.title)
-                                     .frame(width: 50, height: 50)
-                                     .background(
-                                        wrongCharacter.contains(letter) ? Color.red.opacity(0.2) : userGuess.contains(letter) ? Color.green.opacity(0.2) : Color.blue.opacity(0.2))
-                                     .cornerRadius(8)
-                                     .onTapGesture {
-                                         if !wrongCharacter.contains(letter) || !userGuess.contains(letter) {
-                                             onTap(letter: letter)
-                                         }
-                                         
-                                     }
-                              }
-                          }
+                        Text("Each wrong guess will lose 1 heart")
+                            .font(.headline)
+                        Text("You lose when heart each 0")
+                            .font(.headline)
+                    
+                        Button(action: {
+                            getRandomWord()
+                        }, label: {
+                            Text("Started")
+                        })
+                        .padding(.top, 100)
+                        .padding(.bottom, 100)
+                    }
+                    else {
+                        if (guessWord.lowercased() == userGuess.lowercased()) {
+                            Text("You win")
+                        }
+                        else {
+                            VStack {
+                                HStack {
+                                    Spacer()
+                                    Text("❤️ \(life) Remaining")
+                                }.padding(.bottom, 50)
+                                
+                                Text(userGuess)
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .padding(.bottom, 50)
+                                LazyVGrid(columns: columns, spacing: 12) {
+                                      ForEach(characters, id: \.self) { letter in
+                                          Text(letter)
+                                             .font(.title)
+                                             .frame(width: 50, height: 50)
+                                             .background(
+                                                wrongCharacter.contains(letter) ? Color.red.opacity(0.2) : userGuess.contains(letter) ? Color.green.opacity(0.2) : Color.blue.opacity(0.2))
+                                             .cornerRadius(8)
+                                             .onTapGesture {
+                                                 if !wrongCharacter.contains(letter) || !userGuess.contains(letter) {
+                                                     onTap(letter: letter)
+                                                 }
+                                                 
+                                             }
+                                      }
+                                  }
+                                
+                            }
+                        }
                         
                     }
                     
                 }
-                Text(guessWord)
+                else {
+                    Text("You lose")
+                        .font(.title)
+                    Text("The right word is : \(guessWord) ")
+                        .font(.title3)
+                        .fontWeight(.bold)
+      
+                    Button(action: {
+                        tryAgain()
+                    }, label: {
+                        HStack {
+                            Image(systemName: "arrow.clockwise")
+                            Text("Try again")
+                        }
+                    })
+                    .padding(.top, 100)
+                    
+                }
+                
+
                 
             }.padding()
                 .navigationTitle("Guess the word")
