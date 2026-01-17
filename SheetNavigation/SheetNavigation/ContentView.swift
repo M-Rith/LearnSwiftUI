b0084e009e9875e71bccfa1480c87c2eda4d9f7d
USER <user@USERs-MacBook-Pro.local>
2026-01-17 23:42:45 +0900

    add level

---
+++ SheetNavigation/SheetNavigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SheetNavigation
+//
+//  Created by USER on 17/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
